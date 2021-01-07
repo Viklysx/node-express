17e6302cd7daf8e280eaee3780a56747064dfcd7
Viklysx <vikbelec111@yandex.ru>
2021-01-07 17:44:45 +0300

    feat: data rendering

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views'); // указали папку, где хранятся шаблоны
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Главная страница',
+        isHome: true
+    })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Добавить курс',
+        isAdd: true
+    })
+})
+
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: 'Курсы',
+        isCourses: true
+    })
 })
 
 const PORT = process.env.PORT || 3000;
