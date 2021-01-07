221b858632ded39292300aa29885c0f63d70905b
Viklysx <vikbelec111@yandex.ru>
2021-01-07 16:09:14 +0300

    feat: add handlebars

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views'); // указали папку, где хранятся шаблоны
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Start on port ' + PORT);
+});
