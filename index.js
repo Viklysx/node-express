ffcc063638c3b7a38fbb1164bd15bf20634cb976
Viklysx <vikbelec111@yandex.ru>
2021-01-08 04:11:23 +0300

    feat: add mongo

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const mongoose = require('mongoose');
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -17,14 +18,29 @@ app.set('view engine', 'hbs');
 app.set('views', 'views'); // указали папку, где хранятся шаблоны
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
 
 const PORT = process.env.PORT || 3000;
+async function start() {
+    try {
+        const url = 'mongodb+srv://sf-st:gt6DP171IDtdFFRG@cluster0.rp1es.mongodb.net/test?retryWrites=true&w=majority';
+        await mongoose.connect(url, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        app.listen(PORT, () => {
+            console.log('Start on port ' + PORT);
+        });
+    } catch (e) {
+        console.log(e)
+    }
 
-app.listen(PORT, () => {
-    console.log('Start on port ' + PORT);
-});
+}
+
+start();
